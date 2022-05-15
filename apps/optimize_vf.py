14e3aaaa4679572e7bde4ea8d3bb14104f00385a
njindal <njindal@adobe.com>
2022-05-15 22:54:36 +0530

    [OptimizeVF]: SVG for single path

--- apps/optimize_vf.py
+++ apps/optimize_vf.py
@@ -40,23 +40,6 @@ pydiffvg.set_use_gpu(torch.cuda.is_available())
 
 canvas_width, canvas_height = 510, 510
 
-# target_font = TTFont("/content/diffvg/font_data/static/OpenSans-Bold.ttf")
-
-# # transformation
-# target_units_per_em = target_font['head'].unitsPerEm
-# target_ttf2em = transform.Identity.scale(1/target_units_per_em, 1/target_units_per_em)
-# target_svg_per_em=100
-# target_xform = transform.Identity.translate(0, target_svg_per_em).scale(target_svg_per_em, -target_svg_per_em).transform(target_ttf2em)
-
-# # glyph to svg
-# target_glyph_set = target_font.getGlyphSet()
-# target_svgpen = SVGPathPen(target_glyph_set)
-# target_svgpen_transformed = TransformPen(target_svgpen, target_xform)
-# target_glyph = target_glyph_set["C"]
-# target_glyph.draw(target_svgpen_transformed)  
-# target_path = target_svgpen.getCommands()
-
-# https://www.flaticon.com/free-icon/black-plane_61212#term=airplane&page=1&position=8
 target_path = static_font_glyph_svg("/content/diffvg/font_data/static/OpenSans-Bold.ttf", "C")
 target_shapes = pydiffvg.from_svg_path(target_path)
 # print(target_path)
@@ -78,38 +61,11 @@ target_img = render(canvas_width, # width
 pydiffvg.imwrite(target_img.cpu(), 'results/optimize_vf/target.png', gamma=2.2)
 target = target_img.clone()
 
-# print("display_image('results/optimize_vf/target.png')")
-
-# Variable font
-
-# start_variable_font = TTFont("/content/diffvg/font_data/variable/OpenSans.ttf")
-# start_font = mutator.instantiateVariableFont(start_variable_font, {"wght": 300, "wdth": 100})
-
-# # transformation
-# start_units_per_em = start_font['head'].unitsPerEm
-# start_ttf2em = transform.Identity.scale(1/start_units_per_em, 1/start_units_per_em)
-# start_svg_per_em=100
-# start_xform = transform.Identity.translate(0, start_svg_per_em).scale(start_svg_per_em, -start_svg_per_em).transform(start_ttf2em)
-
-# # glyph to svg
-# start_glyph_set = start_font.getGlyphSet()
-# start_svgpen = SVGPathPen(start_glyph_set)
-# start_svgpen_transformed = TransformPen(start_svgpen, start_xform)
-# start_glyph = start_glyph_set["C"]
-# start_glyph.draw(start_svgpen_transformed)  
-# start_path = start_svgpen.getCommands()
-
-# Move the path to produce initial guess
-# normalize points for easier learning rate
-# noise = torch.FloatTensor(shapes[0].points.shape).uniform_(0.0, 1.0)
 start_axis_value = torch.tensor(300.0, requires_grad=True)
 print({"wght": start_axis_value.item(), "wdth": 100})
 start_path = variable_font_glyph_svg("/content/diffvg/font_data/variable/OpenSans.ttf", "C", {"wght": start_axis_value.item(), "wdth": 100})
 start_shapes = pydiffvg.from_svg_path(start_path)
-# start_points_n = start_shapes[0].points.clone()
-# start_points_n.requires_grad = True
 color = torch.tensor([0.3, 0.2, 0.5, 1.0], requires_grad=True)
-# start_shapes[0].points = start_points_n
 start_path_group = pydiffvg.ShapeGroup(shape_ids = torch.tensor([0]),
                                  fill_color = color)
 start_shape_groups = [start_path_group]
